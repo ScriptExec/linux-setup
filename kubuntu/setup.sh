4ea97a08fba26e8c6a127cacfe3469d1b3a25538
ScriptExec <scriptexec.contact@gmail.com>
2025-10-07 19:43:32 +0200

    Replaced zsh-syntax-highlighting install

--- kubuntu/setup.sh
+++ kubuntu/setup.sh
@@ -1,7 +1,8 @@
 # Setup zsh
-sudo apt install zsh zsh-syntax-highlighting
+sudo apt install zsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 cp ../common/.zshrc ~/.zshrc
 
 sudo apt install -y build-essential git
